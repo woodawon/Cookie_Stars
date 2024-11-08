05f4f37de97499df57901ca63aab7deba364f0a5
woodawon <woodawon0602@gmail.com>
2024-11-08 15:52:15 +0900

    1108.3

---
+++ static/js/detail_UserInput.js
@@ -0,0 +1,8 @@
+export default function UserInput() {
+    return `
+        <footer id="user-input">
+            <textarea id="message-input" placeholder="AI 챗봇과 상의하면서 표를 작성해보셔도 좋아요!" style="height : 55px;"></textarea>
+            <button id="send-btn">submit</button>
+        </footer>
+    `;
+}
