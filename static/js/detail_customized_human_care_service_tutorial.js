9db63def3464a9d86a27deca217504f257d6dc42
woodawon <woodawon0602@gmail.com>
2024-11-21 16:29:46 +0900

    11.21-2

---
+++ static/js/detail_customized_human_care_service_tutorial.js
@@ -0,0 +1,4 @@
+document.getElementById("p_finish").addEventListener("click", function() {
+  const tutorialDiv = document.getElementById("tutorial");
+  tutorialDiv.style.display = "none"; // 튜토리얼 창 숨기기
+});
